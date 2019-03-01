c8d6359274096fc08b049d5c5332fcbccee6a872
Avinash Thangali <avinashthangali@gmail.com>
2019-03-01 12:34:57 -0500

    compile everything into one module

--- backend/app.py
+++ backend/app.py
@@ -21,6 +21,54 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # initialize the database connection
 db = SQLAlchemy(app)
+
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    points = db.Column(db.Integer)
+
+    def __init__(self, name=None, points=0):
+        self.name = name
+        self.points = points
+
+
+class Corner(db.Model):
+
+    __tablename__ = 'corners'
+    id = db.Column(db.Integer, primary_key=True)
+    lat = db.Column(db.Float)
+    lon = db.Column(db.Float)
+    street1 = db.Column(db.String(80), primary_key=True)
+    street2 = db.Column(db.String(80), primary_key=True)
+
+    def __init__(self, street1=None, street2=None, lat=None, lon=None):
+        self.street1 = street1
+        self.street2 = street2
+        self.lat = lat
+        self.lon=lon
+
+class Claims(db.Model):
+
+    __tablename__ = 'claims'
+    id = db.Column(db.Integer, primary_key=True)
+    state = db.Column(db.Integer)
+    before_pic = db.Column(db.PickleType)
+    after_pic = db.Column(db.PickleType)
+    start = db.Column(db.DateTime)
+    end = db.Column(db.DateTime)
+
+    def __init__(self, state=None, before_pic=None, after_pic=None, start=None, end=None):
+        self.state = state
+        self.before_pic = before_pic
+        self.after_pic = after_pic
+        self.start = start
+        self.end = end
+
+
+
 # db.init_app(app)
 db.create_all()
 db.session.commit()
@@ -33,13 +81,11 @@ migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
-    from models import User
     print(User.query.all())
     return 'works'
 
 @app.route('/register', methods=['POST'])
 def register_user():
-    from models import User
     name = request.form.get('name')
     usr = User(name)
     db.session.add(usr)
