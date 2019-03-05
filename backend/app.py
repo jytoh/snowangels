eaaec1e28c2a3baa108d4cbf3b70fa9c19b88c52
Avinash Thangali <avinashthangali@gmail.com>
2019-03-04 21:46:20 -0500

    all tables finished, just have to do routes now

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-import os
+import os, datetime
 
 from flask import Flask, render_template, request
 from flask_migrate import Migrate
@@ -23,16 +23,54 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+class Subscription(db.Model):
+    __tablename__='subscriptions'
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    corner_id = db.Column(db.Integer, db.ForeignKey("corners.id"))
+
+class Request(db.Model):
+    __tablename__='requests'
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    corner_id = db.Column(db.Integer, db.ForeignKey("corners.id"))
+    time = db.Column(db.DateTime)
+    state = db.Column(db.Integer)
+
+    def __init__(self):
+        self.time = datetime.datetime.now()
+
 class User(db.Model):
 
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    points = db.Column(db.Integer)
+    point = db.relationship(
+        "Point", backref="user", lazy="select", uselist=False
+    )
 
-    def __init__(self, name=None, points=0):
+    subscription = db.relationship(
+        "Subscription", backref="user", lazy="select", uselist=True
+    )
+
+    request = db.relationship(
+        "Request", backref="user", lazy="select", uselist=True
+    )
+
+    shoveling = db.relationship(
+        "Shoveling", backref="user", lazy="select", uselist=True
+    )
+
+    def __init__(self, name=None):
         self.name = name
-        self.points = points
+
+class Point(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    day_pts = db.Column(db.Integer)
+    week_pts = db.Column(db.Integer)
+    szn_pts = db.Column(db.Integer)
+    after_pics = db.Column(db.PickleType)
 
 
 class Corner(db.Model):
@@ -41,8 +79,19 @@ class Corner(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     lat = db.Column(db.Float)
     lon = db.Column(db.Float)
-    street1 = db.Column(db.String(80), primary_key=True)
-    street2 = db.Column(db.String(80), primary_key=True)
+    street1 = db.Column(db.String(80))
+    street2 = db.Column(db.String(80))
+    subscription = db.relationship(
+        "Subscription", backref="corner", lazy="select", uselist=True
+    )
+
+    request = db.relationship(
+        "Request", backref="corner", lazy="select", uselist=True
+    )
+
+    shoveling = db.relationship(
+        "Shoveling", backref="corner", lazy="select", uselist=True
+    )
 
     def __init__(self, street1=None, street2=None, lat=None, lon=None):
         self.street1 = street1
@@ -50,15 +99,17 @@ class Corner(db.Model):
         self.lat = lat
         self.lon=lon
 
-class Claims(db.Model):
+class Shoveling(db.Model):
 
-    __tablename__ = 'claims'
+    __tablename__ = 'shovelings'
     id = db.Column(db.Integer, primary_key=True)
-    state = db.Column(db.Integer)
     before_pic = db.Column(db.PickleType)
     after_pic = db.Column(db.PickleType)
     start = db.Column(db.DateTime)
     end = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    corner_id = db.Column(db.Integer, db.ForeignKey("corners.id"))
+
 
     def __init__(self, state=None, before_pic=None, after_pic=None, start=None, end=None):
         self.state = state
@@ -67,6 +118,10 @@ class Claims(db.Model):
         self.start = start
         self.end = end
 
+
+# COMMENT THIS OUT WHEN DEPLOYING
+db.reflect()
+db.drop_all()
 
 
 # db.init_app(app)
@@ -81,7 +136,11 @@ migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
-    print(User.query.all())
+    print(Corner.query.all())
+    print(Point.query.all())
+    print(Subscription.query.all())
+    print(Request.query.all())
+    print(Shoveling.query.all())
     return 'works'
 
 @app.route('/register', methods=['POST'])
@@ -90,6 +149,7 @@ def register_user():
     usr = User(name)
     db.session.add(usr)
     db.session.commit()
+
 
 if __name__ == "__main__":
     app.run()
