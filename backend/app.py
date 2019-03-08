24178e9cf866d0287156c1e963625745556bdf74
Mishcat <mi243@cornell.edu>
2019-03-07 20:13:36 -0500

    addded validate shovel endpoint

--- backend/app.py
+++ backend/app.py
@@ -256,13 +256,46 @@ def new_shovel():
     db.session.commit()
     return "User %s has claimed to shovel Corner %s" % (uid, cid)
 
+#validate shoveling
+@app.route("/validate_shovel", methods=['POST'])
+def validate_shovel():
+    uid_requester = request.form["uid_requester"]
+    uid_shoveler = request.form["uid_shoveler"]
+    cid = request.form["cid"]
+    validate_bit = request.form["validate_bit"]
+    shoveler = User.query.get(uid_shoveler)
+    corner = Corner.query.get(cid)
+    #if requester says shoveling claim is not valid, take away points from shoveler + set state of request to 0
+    if validate_bit=='0':
+        points_entry = Point.query.filter_by(user_id=uid_shoveler).first()
+        points_entry.day_pts -= 5 #TODO: figure out good # of points/ weights later
+        points_entry.week_pts -= 5
+        points_entry.szn_pts -= 5
+        db.session.commit()
+        req = Request.query.filter_by(corner_id=cid, state=1, user_id=uid_requester).order_by(Request.time.desc()).first()
+        req.state = 0 #TODO: need to re-notify ppl that this corner needs to be cleared
+        db.session.commit()
+        return "User %s calimed that user %s did not properly shovel Corner %s" % (uid_requester, uid_shoveler, cid)
+    #if requester says shoveling claim is valid, set state of request to steady state, 2
+    elif validate_bit=='1':
+        req = Request.query.filter_by(corner_id=cid, state=1, user_id=uid_requester).order_by(Request.time.desc()).first()
+        req.state = 2 
+        db.session.commit()
+        return "User %s validated that user %s shoveled Corner %s" % (uid_requester, uid_shoveler, cid)
+
+#get all people subscribed to a corner
+@app.route("/ppl_subscribed", methods=['GET'])
+def get_ppl_subscribed():
+    cid = request.form["cid"]
+    users_subscribed = map(str,[s.user_id for s in Subscription.query.filter_by(corner_id=cid)])
+    return "Users %s are subscribed to corner %s" % (' '.join(users_subscribed), cid)
 
 #get corners that user is subscribed to
 @app.route("/subscribed_corners", methods=['GET'])
 def get_subscribed_corners():
     uid = request.form["uid"]
     corners = map(str,[s.corner_id for s in Subscription.query.filter_by(user_id=uid)])
-    return ' '.join(corners)
+    return "User %s is subscribed to corners %s" % (uid, ' '.join(corners)) 
 
 #unsubscribe a user from a corner
 @app.route("/unsubscribe_corner", methods=['DELETE'])
