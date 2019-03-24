96ff31afd7b8a06fbb6cade12451148f60ee9c94
Jeen Toh <jeen@dhcp-gs-3414.eduroam.cornell.edu>
2019-03-24 14:41:20 -0400

    change port to 5432

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ POSTGRES = {
     'pw': 'password',
     'db': 'template1', #had to change this bc I couldnt add a db
     'host': 'localhost',
-    'port': '5433',
+    'port': '5432',
 }
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
 %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
