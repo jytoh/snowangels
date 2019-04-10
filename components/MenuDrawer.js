52108a0c39dc27336d930e0a17a67049e8979286
Mishcat <mi243@cornell.edu>
2019-04-10 17:16:54 -0400

    small change to sidebar

--- components/MenuDrawer.js
+++ components/MenuDrawer.js
@@ -33,6 +33,9 @@ export default class MenuDrawer extends React.Component {
                     <View style = {styles.bottomLinks}>
                         {this.navLink('Home', 'Home')}
                         {this.navLink('Profile', 'Profile')}
+                        {this.navLink('Leaderboard', 'Leaderboard')}
+                        {this.navLink('Links', 'Links')}
+                        {this.navLink('History', 'History')}
                         {this.navLink('Settings', 'Settings')}
                         {this.navLink('Camera','Camera')}
                     </View>
