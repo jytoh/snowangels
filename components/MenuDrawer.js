793563918ba6050a02a9eed82b281aacec7a0b00
Jiahua Li <jl3253@cornell.edu>
2019-04-10 17:11:27 -0400

    updated sidebar

--- components/MenuDrawer.js
+++ components/MenuDrawer.js
@@ -1,43 +1,44 @@
 import React from 'react';
-import { View, Text, Image, ScrollView, Platform, Dimensions, StyleSheet, TouchableOpacity } from 'react-native'
+import{ View, Text, Image, ScrollView, Platform, Dimensions, StyleSheet, TouchableOpacity } from 'react-native'
 
 const WIDTH = Dimensions.get('window').width
 const HEIGHT = Dimensions.get('window').height
 
 export default class MenuDrawer extends React.Component {
     navLink(nav, text) {
-        return (
-            <TouchableOpacity style={{ height: 50 }} onPress={() => this.props.navigation.navigate(nav)}>
+        return(
+            <TouchableOpacity style = {{height: 50}} onPress = {() => this.props.navigation.navigate(nav)}>
                 <Text style={styles.link}>{text}</Text>
             </TouchableOpacity>
         )
     }
     render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.topLinks}>
+        return(
+            <View style = {styles.container}>
+                <View style = {styles.topLinks}>
                     <View style={styles.profile}>
-                        <View style={styles.imgView}>
-                            <Image style={styles.img} source={require('../assets/sarah.jpeg')} />
+                        <View style ={styles.imgView}>
+                            <TouchableOpacity onPress={() => this.props.navigation.navigate('Profile')}>
+                            <Image style = {styles.img} source={require('../assets/sarah.jpeg')}/>
+                            </TouchableOpacity>
                         </View>
-                        <View style={styles.profileText}>
-                            <Text style={styles.name}> User </Text>
+                        <View style = {styles.profileText}>
+                            <TouchableOpacity onPress={() => this.props.navigation.navigate('Profile')}>
+                            <Text style = {styles.name}> User </Text>
+                            </TouchableOpacity>
                         </View>
                     </View>
                 </View>
                 <ScrollView style={styles.scroller}>
-                    <View style={styles.bottomLinks}>
+                    <View style = {styles.bottomLinks}>
                         {this.navLink('Home', 'Home')}
                         {this.navLink('Profile', 'Profile')}
-                        {this.navLink('Leaderboard', 'Leaderboard')}
-                        {this.navLink('Links', 'Links')}
-                        {this.navLink('History', 'History')}
                         {this.navLink('Settings', 'Settings')}
                         {this.navLink('Camera','Camera')}
                     </View>
                 </ScrollView>
                 <View style={styles.footer}>
-                    <Text style={styles.description}>Menu Practice</Text>
+                    <Text style={styles.description}>Snow Angels</Text>
                     <Text style={styles.version}>v1.0</Text>
                 </View>
             </View>
@@ -48,7 +49,7 @@ export default class MenuDrawer extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'lightgray',
+        backgroundColor: '#E1EAFB',
     },
     scroller: {
         flex: 1
@@ -66,29 +67,29 @@ const styles = StyleSheet.create({
         width: 90,
         borderRadius: 40,
     },
-    imgView: {
+    imgView:{
         flex: 3,
         paddingLeft: 20,
         paddingRight: 20,
     },
-    profileText: {
+    profileText:{
         flex: 3,
         flexDirection: 'column',
         justifyContent: 'center',
     },
-    name: {
+    name:{
         fontSize: 20,
         paddingBottom: 5,
         color: 'white',
         textAlign: 'left',
     },
-    topLinks: {
+    topLinks:{
         height: 160,
-        backgroundColor: 'black',
+        backgroundColor: '#76A1EF',
     },
-    bottomLinks: {
+    bottomLinks:{
         flex: 1,
-        backgroundColor: 'white',
+        backgroundColor: '#E1EAFB',
         paddingTop: 10,
         paddingBottom: 450,
     },
@@ -100,7 +101,7 @@ const styles = StyleSheet.create({
         margin: 5,
         textAlign: 'left',
     },
-    footer: {
+    footer:{
         height: 50,
         flexDirection: 'row',
         alignItems: 'center',
