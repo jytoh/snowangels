81a02e9a986d373fef4b63874c29d4307452f099
martineizayaga <martineizayaga@gmail.com>
2019-04-30 16:56:37 -0400

    quadri and martin uris lib

--- components/ListOfShovels.js
+++ components/ListOfShovels.js
@@ -2,30 +2,7 @@ import React from 'react';
 import { AppRegistry, FlatList, StyleSheet, Text, View } from 'react-native';
 import { Icon, ListItem } from "react-native-elements";
 import TouchableScale from 'react-native-touchable-scale';
-
-const list = [
-  {
-    name: 'March 20th, 2019 4:25 PM',
-    subtitle: "N Aurora St & E Court Street",
-    icon: 'snowflake-o'
-  },
-  {
-    name: 'Febuary 4th, 2019 1:36 PM',
-
-    subtitle: "N Aurora St & E Court Street",
-    icon: 'snowflake-o'
-  },
-  {
-    name: 'January 12th, 2019 9:25 AM',
-    subtitle: "Stewart Ave & University Ave",
-    icon: 'snowflake-o'
-  },
-  {
-    name: 'December 19th, 2018 11:15 AM',
-    subtitle: "Stewart Ave & University Ave",
-    icon: 'snowflake-o'
-  }
-]
+import { SecureStore } from 'expo';
 
 const list2 = [
   {
@@ -78,26 +55,19 @@ export default class ListOfShovels extends React.Component {
   componentDidMount() {
     this.fetchData();
   }
-
+ 
   fetchData = async () => {
     const response = await fetch("https://snowangels-api.herokuapp.com/get_user_history");
     const json = await response.json();
     console.log(json);
     console.log("/////");
-    this.setState({ data: json });
+    var user_id = await SecureStore.getItemAsync('id');
+    console.log("USER ID");
+    console.log(user_id);
+    var userShovels = this.getSpecificUserShovels(json, 2);
+    this.setState({ data: userShovels });
     //console.log(this.setState);
   }
-
-
-
-  // getSpecificUser(arr, uid) {
-  //   user_shovels = [];
-  //   for (let userObject of arr) {
-  //     if (userObject.uid == uid) {
-
-  //     }
-  //   }
-  // }
 
   renderItem = ({ item }) => (
     < ListItem
@@ -148,19 +118,6 @@ export default class ListOfShovels extends React.Component {
         style={{ width: 375 }}
         ItemSeparatorComponent={this.renderSeparator}
       />
-
-      // <View>
-      //   {
-      //     list.map((item, i) => (
-      //       <ListItem
-      //         key={i}
-      //         title={item.title}
-      //         leftIcon={{ name: item.icon }}
-      //       />
-      //     ))
-      //   }
-      // </View>
-
 
     )
   }
