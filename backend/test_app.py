f2a2b878fe0821bb8a55520b4a5e0220d6678893
Avinash Thangali <avinashthangali@gmail.com>
2019-05-16 16:56:33 -0400

    add some tests

--- backend/test_app.py
+++ backend/test_app.py
@@ -1,9 +1,40 @@
-import requests, pytest
+import requests, pytest, random
 
 
-r=requests.post('https://snowangels-api.herokuapp.com/create_corners_from_filename',data={'filename':'Ints2019 copy.xls'})
-#r=requests.get('https://snowangels-api.herokuapp.com/get_all_corners')
-#r=requests.get('https://snowangels-api.herokuapp.com/register/testuser', data ={})
-#r=requests.post('https://snowangels-api.herokuapp.com/new_shovel', data ={'cid':'10', 'uid':'1', 'after_pic':'aaa'})
-#r=requests.get('https://snowangels-api.herokuapp.com/')
+# def test_add_corners():
+#     r=requests.post('https://snowangels-api.herokuapp.com'
+#                 '/create_corners_from_filename',data={'filename':'Ints2019.xls',
+#                                                       'should_clear': "true"})
+
+# def test_num_corners():
+#     r = requests.get('https://snowangels-api.herokuapp.com/get_all_corners')
+#     assert len(r.json()) == 505
+
+def test_reg_user():
+    gid = (random.randint(1, 1000))
+    r = requests.post('https://snowangels-api.herokuapp.com/register_user',
+                     data={'name': "Avinash", "google_id":gid,
+                           "photourl":" ",
+                           "token":" "})
+
+    assert r.ok
+
+# def test_new_shovel_and_request():
+#     r = requests.get('https://snowangels-api.herokuapp.com/get_all_corners')
+#     cid = int(r.json()[0].get('key'))
+#     r = requests.post('https://snowangels-api.herokuapp.com/new_request',
+#                       data={ "uid": 3, "cid": 2043, "after_pic": "aaa"})
+#     assert r.ok
+#     r = requests.post('https://snowangels-api.herokuapp.com/new_shovel',
+#                   data={'cid': 2043, 'uid': 3, 'after_pic': 'aaa'})
+#     assert r.ok
+
+def test_corner_st_names():
+    r = requests.get('https://snowangels-api.herokuapp.com/corner_street_names')
+    assert r.ok
+    assert len(r.json()) > 0
+
+
+
+
 #r=requests.get('https://snowangels-api.herokuapp.com/top_day_leader_ids')
